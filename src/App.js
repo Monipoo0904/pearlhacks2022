5b904ff6252c7857c3d19ab82fff34b95c60d4a6
Monique <moniquereed1011@outlook.com>
2022-02-19 18:17:38 -0500

    adding tons of files for auth0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
-function App() {
+// import { navbar } from './components';
+import Home from './views/Home';
+
+const App = () => {
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src='https://i.pinimg.com/474x/75/4a/7d/754a7d80ff30b6dcb2df5423ac60cec2.jpg' className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <button className='button'>Sign Up</button>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        
       </header>
+
+      <Routes>
+          <Route path="/" exact component={Home} />
+         
+       </Routes>
+
     </div>
   );
 }
