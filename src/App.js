1adc48a659b84dd780de8acbb36d8fd3d630d88b
Monique <moniquereed1011@outlook.com>
2022-02-20 08:10:24 -0500

    bringing back the nav lol

--- src/App.js
+++ src/App.js
@@ -17,15 +17,15 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-       {/* <NavBar /> */}
+       <NavBar />
 
         <img src='https://i.pinimg.com/474x/75/4a/7d/754a7d80ff30b6dcb2df5423ac60cec2.jpg' className="App-logo" alt="logo" />
-        <>
+        {/* <>
         <LoginButton/>
         <LogoutButton/>
         <Profile/>
 
-        </>
+        </> */}
    <h3>What's to come!!!</h3>
   <img  src="phone.png" className='phone' alt='whats to come'></img> 
       </header>
